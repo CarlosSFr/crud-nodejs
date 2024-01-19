e8907ba7d9b359fc87ebbceac150ea540b8f2266
Carlos Freitas <carlos.edfrei@gmail.com>
2024-01-18 22:27:40 -0300

    versão 1.0.0

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const button = document.getElementById('login');
 
 button.addEventListener('click', () => {
-    
-    window.location.href = "./pages/home/home.html";
+
+  window.location.href = "/pages/home/home.html"
 
 });
-
-    
-
---
+++ pages/home/home.js
@@ -0,0 +1,7 @@
+const logout = document.getElementById("icon-logout");
+
+logout.addEventListener('click', () => {
+        
+        window.location.href = "../../index.html";
+
+});
