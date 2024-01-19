85a212890995a322ccbd21058d6010bed631146b
Carlos Eduardo Santos Freitas <carlos.freitas@valmetpartners.com>
2024-01-19 17:00:39 -0300

    versão 1.0.2

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-const button = document.getElementById('login');
+const buttonLogin = document.getElementById('login');
+const email = document.getElementById('email').value;
+const password = document.getElementById('password').value;
 
-button.addEventListener('click', () => {
+buttonLogin.addEventListener('click', () => {
     
     window.location.href = "./pages/home/home.html";
 
 });
 
-    
+function validateEmailAndPassword(){
+
+    if(!email){
+        buttonLogin.disabled = "true";
+    }else if(validateEmail(email)){
+        buttonLogin.disabled = "false";
+    }else{
+        buttonLogin.disabled = "true";
+    }
+
+}
+
+function validateEmail(email){
+    return /\S+@\S+\.\S+/.test(email);
+}
+
+
+
+
+firebase.auth().signInWithEmailAndPassword("carlos.edfrei@gmail.com", "teste123").then(
+    response => {
+        console.log("success", response);
+}).catch(error => {
+        console.log("error", error);
+});
 
