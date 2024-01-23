e7d340b59bdfaae7a5a50aacdd0eb7466c54798c
Carlos Freitas <carlos.edfrei@gmail.com>
2024-01-22 22:22:28 -0300

    teste

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-const buttonLogin = document.getElementById('login');
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
 
-buttonLogin.addEventListener('click', () => {
-    
-    window.location.href = "./pages/home/home.html";
 
-});
+// document.getElementById("login").addEventListener("click" , () => {
 
-function validateEmailAndPassword(){
+//     window.location.href = "pages/home/home.html";
 
-    if(!email){
-        buttonLogin.disabled = "true";
-    }else if(validateEmail(email)){
-        buttonLogin.disabled = "false";
-    }else{
-        buttonLogin.disabled = "true";
-    }
+// });
+
+function validateFields() {
+    const emailValid = isEmailValid();
+    document.getElementById("forgotten-password").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login").disabled = !emailValid || !passwordValid;
 
 }
 
-function validateEmail(email){
+function isEmailValid() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        return false;
+    }
+    return validateEmail(email);
+}
+
+function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+        return false;
+    }
+    return true;
+}
+
+function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
-
-
-
 
 firebase.auth().signInWithEmailAndPassword("carlos.edfrei@gmail.com", "teste123").then(
     response => {
         console.log("success", response);
-}).catch(error => {
+    }).catch(error => {
         console.log("error", error);
-});
+    });
+
+
+
 
